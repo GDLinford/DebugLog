a4a6933f78940de9336fe5abeb602ea7f51d4221
GDLinford <55755896+GDLinford@users.noreply.github.com>
2019-10-02 11:41:47 +0100

    DebugLog work from october 2nd lecture

---
+++ Debug Message/Assets/Scripts/DebugLog.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugLog : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Debug.Log("GRIM REAPER");
+       
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Moon"); 
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log("GoodBye World");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("See you soon!");
+    }
+
+
+}
